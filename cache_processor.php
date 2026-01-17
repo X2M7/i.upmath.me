30007337568d8605ab95584392a2976d02547a25
Xumin Liang <liang.xumin97@gmail.com>
2026-01-18 01:49:12 +0800

    Update cache_processor.php
    ?c=<html color>

--- cache_processor.php
+++ cache_processor.php
@@ -1,20 +1,18 @@
 <?php
 /**
- * Entry point for async cache optimizer.
- *
- * @copyright 2020-2022 Roman Parpalak
- * @license   http://www.opensource.org/licenses/mit-license.php MIT
- * @package   Upmath Latex Renderer
- * @link      https://i.upmath.me
- */
-
+* Entry point for async cache optimizer.
+*
+* @copyright 2020-2022 Roman Parpalak
+* @license   http://www.opensource.org/licenses/mit-license.php MIT
+* @package   Upmath Latex Renderer
+* @link      https://i.upmath.me
+*/
 require 'vendor/autoload.php';
 require 'config.php';
 
 // Fallback commands, now HTTP service is used.
 define('SVGO', realpath(SVGO_PATH) . '/svgo -i %1$s -o %1$s.new; rm %1$s; mv %1$s.new %1$s');
 define('GZIP', 'gzip -cn6 %1$s > %1$s.gz.new; rm %1$s.gz; mv %1$s.gz.new %1$s.gz; rm %1$s');
-
 // outdated, disabled due to SVG is now well-supported in browsers.
 define('OPTIPNG', 'optipng %1$s');
 define('PNGOUT', 'pngout %1$s');
@@ -24,16 +22,22 @@ use S2\Tex\Processor\DelayedProcessor;
 use S2\Tex\Processor\Request;
 
 $delayedProcessor = new DelayedProcessor(
-	new CacheProvider(CACHE_SUCCESS_DIR, CACHE_FAIL_DIR),
-	'http://localhost:' . (defined('HTTP_SVGO_PORT') ? HTTP_SVGO_PORT : '8800') . '/'
+    new CacheProvider(CACHE_SUCCESS_DIR, CACHE_FAIL_DIR),
+    'http://localhost:' . (defined('HTTP_SVGO_PORT') ? HTTP_SVGO_PORT : '8800') . '/'
 );
 
 $delayedProcessor
-	->addSVGCommand(SVGO)
-	->addSVGCommand(GZIP)
-//	->addPNGCommand(OPTIPNG)
-//	->addPNGCommand(PNGOUT)
+    ->addSVGCommand(SVGO)
+    ->addSVGCommand(GZIP)
+// ->addPNGCommand(OPTIPNG)
+// ->addPNGCommand(PNGOUT)
 ;
 
-$request = new Request($_POST['formula'], $_POST['extension']);
+$formula = $_POST['formula'] ?? '';
+$ext     = $_POST['extension'] ?? 'svg';
+
+// IMPORTANT: same variant logic as render.php/request parsing
+$variant = Request::buildVariantFromParams($_POST);
+
+$request = new Request($formula, $ext, $variant);
 $delayedProcessor->process($request);
